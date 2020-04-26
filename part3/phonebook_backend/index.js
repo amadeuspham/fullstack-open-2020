9e4d1c9cf599f6205f59d333b4ea31cf26a1e49e
hunganh <scramblegf@gmail.com>
2020-04-26 16:50:41 +0300

    Submit 3b

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express()
 var morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
 	const person = req.body
@@ -99,7 +102,7 @@ app.get('/api/info', (req, res) => {
   res.send(info + date)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
